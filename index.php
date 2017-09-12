8ddd8d25cbd75d388814f2cce6bda08e57bcb6cc
gabrielsanna <gabrielsanna1@gmail.com>
2017-09-12 19:04:32 -0400

    Initial commit of index.php file

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Gabriel Sanna</title>
+		<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+	</head>
+	<body>
+		<div id="wrapper">
+
+			<h1>Gabriel Sanna</h1>
+
+			<img src='images/john-gosier.jpg' alt='Gabriel Sanna profile photo'>
+
+			<h2>About Me</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+			<h2>Random Quote</h2>
+			<blockquote>
+				Do your own thing on your own terms and get what you came here for. -Oliver James
+			</blockquote>
+		</div>
+	</body>
+</html>
