6b37ecb11df4a5460e4a9a0cdcf91f4448892d54
gabrielsanna <gabrielsanna1@gmail.com>
2017-09-12 20:57:54 -0400

    Added random quote generator and more styles

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+$quote_list=array(
+	"\"The number one benefit of information technology is that it empowers people to do what they want to do. It lets people be creative. It lets people be productive. It lets people learn things they didn't think they could learn before, and so in a sense it is all about potential.\"<br><br>—Steve Ballmer",
+	"\"Technology is just a tool. In terms of getting the kids working together and motivating them, the teacher is the most important.\"<br><br>—Bill Gates",
+	"\"I'm not a tech guy. I'm looking at the technology with the eyes of my customers, normal people's eyes.\"<br><br>—Jack Ma",
+	"\"If it isn’t on Google, it doesn’t exist.\"<br><br>—Jimmy Wales"
+);
+
+$random_quote=$quote_list[array_rand($quote_list)]
+
+?>
--- index.php
+++ index.php
@@ -3,22 +3,25 @@
 <html>
 	<head>
 		<title>Gabriel Sanna</title>
+
 		<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="styles.css">
 	</head>
 	<body>
+		<?php include 'functions.php';?>
+		
 		<div id="wrapper">
 
 			<h1>Gabriel Sanna</h1>
 
-			<img src='images/john-gosier.jpg' alt='Gabriel Sanna profile photo'>
+			<img src='images/gsanna.jpg' alt='Gabriel Sanna profile photo' id="photo">
 
 			<h2>About Me</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+			<p>I'm a systems administrator at a software company outside of Boston, and a student here at the Extension School! I've lived here in Boston for three years, and love it. In my spare time, I try to learn and read, rock climb, see movies, and hang out with my girlfriend's dog.</p>
 
 			<h2>Random Quote</h2>
 			<blockquote>
-				Do your own thing on your own terms and get what you came here for. -Oliver James
+				<?php echo $random_quote ?>
 			</blockquote>
 		</div>
 	</body>
